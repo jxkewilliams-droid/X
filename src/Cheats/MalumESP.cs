375e6b485aace095831b700e0221350993335d45
jxkewilliams-droid <jxkewilliams@gmail.com>
2026-01-17 08:53:47 +0000

    Update MalumESP.cs

--- src/Cheats/MalumESP.cs
+++ src/Cheats/MalumESP.cs
@@ -6,6 +6,7 @@ public static class MalumESP
 {
     public static bool freecamActive;
     public static bool resolutionchangeNeeded;
+    public static bool z;
     public static void sporeCloudVision(Mushroom mushroom)
     {
         if (CheatToggles.fullBright)
@@ -154,6 +155,29 @@ public static class MalumESP
                 Camera.main.gameObject.GetComponent<FollowerCamera>().SetTarget(PlayerControl.LocalPlayer);
                 freecamActive = false;
 
+            }
+        }
+    }
+    public static void x(HudManager hudManager)
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            if (z)
+            {
+            Camera.main.gameObject.GetComponent<FollowerCamera>().enabled = false;
+            Camera.main.gameObject.GetComponent<FollowerCamera>().Target = null;
+            Camera.main.transform.position = new Vector3(20.5f, -13.0f, 0.0f);
+            Camera.main.orthographicSize = 13.8f;
+            hudManager.UICamera.orthographicSize = 13.8f;
+            Utils.adjustResolution();
+            z = false;
+            }else{
+            Camera.main.gameObject.GetComponent<FollowerCamera>().enabled = true;
+            Camera.main.gameObject.GetComponent<FollowerCamera>().SetTarget(PlayerControl.LocalPlayer);
+            Camera.main.orthographicSize = 3f;
+            hudManager.UICamera.orthographicSize = 3f;
+            Utils.adjustResolution();
+            z = true;
             }
         }
     }
